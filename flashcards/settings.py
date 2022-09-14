48b0e18a0d9bba21c8479a8d7e6e69c084fcc794
jerrytigerxu <jere.tiger.xu@gmailc.om>
2022-09-14 16:53:28 -0400

    postgresql

--- flashcards/settings.py
+++ flashcards/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'flashcards.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'flashcards',
+        'USER': 'jerrytigerxu',
+        'PASSWORD': 'rabbit168Win',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
